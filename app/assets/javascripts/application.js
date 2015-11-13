ff8450711e75cc96d358893b228a6cbc35256e62
Samir Mehta <sammehta88@gmail.com>
2015-11-13 14:59:38 -0500

    remove has:one relation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+// = require jquery
+// = require jquery_ujs
+// = require turbolinks
+// = require_tree .
+
+$(document).ready(function(){
+  $('button').click(function(){
+    alert('JS on RAILS');
+  });
+});
